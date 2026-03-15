96daf6b2f561ce7feb5c2788e0a96f95b8591510
kahuatek <kahuatektruly@gmail.com>
2026-03-15 23:23:46 +0100

    data!

--- Scripts/SheetsScript.js
+++ Scripts/SheetsScript.js
@@ -1,1 +1,18 @@
 //SheetsScript.js
+
+export async function fetchSheetData(sheetID, sheetName) {
+    var res = null;
+    const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=${sheetName}`;
+
+    try {
+        const response = await fetch(url);
+        const text = await response.text();
+
+        const json = JSON.parse(text.substring(47).slice(0, -2));
+
+        res = json.table;
+    } catch (error) {
+        console.error(error);
+    }
+    return res; 
+}
--- Scripts/pageMainScript.js
+++ Scripts/pageMainScript.js
@@ -1,1 +1,12 @@
 //pageMainScript.js
+import { fetchSheetData } from './SheetsScript.js';
+const config = fetch('../config.json');
+
+data = fetchSheetData(config.sheet_config.main_sheet.id, config.sheet_config.main_sheet.markers)
+
+function text(id) {
+    const table = document.getElementById(id);
+    table.innerText = data;
+}
+
+text("map")
