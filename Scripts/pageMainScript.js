b89fdb632f635155e1e580c0b60d6048ba5b11d5
kahuatek <kahuatektruly@gmail.com>
2026-03-15 23:39:16 +0100

    4

--- Scripts/pageMainScript.js
+++ Scripts/pageMainScript.js
@@ -3,7 +3,7 @@ import { fetchSheetData } from './SheetsScript.js';
 
 async function text(id) {
     const config = await fetch('./config.json'); 
-    const data = await fetchSheetData(config.sheet_config.main_sheet.id, config.sheet_config.main_sheet.markers)
+    const data = await fetchSheetData("1Qv5yAojhOPR259mfrXL3qha0HiVgYIBzt2o4JgZeZFc", "markers")
 
     const table = document.getElementById(id);
     table.innerText = data;
