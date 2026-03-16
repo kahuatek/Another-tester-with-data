aecb9771613f00b99a90455088b53a84df51c9b0
kahuatek <kahuatektruly@gmail.com>
2026-03-16 10:29:48 +0100

    Update SheetsScript.js

--- Scripts/SheetsScript.js
+++ Scripts/SheetsScript.js
@@ -1,18 +1,28 @@
 //SheetsScript.js
-
 export async function fetchSheetData(sheetID, sheetName) {
-    var res = null;
     const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=${sheetName}`;
 
     try {
         const response = await fetch(url);
         const text = await response.text();
-
         const json = JSON.parse(text.substring(47).slice(0, -2));
 
-        res = json.table;
+        const table = json.table;
+
+        const attributes = table.rows[0].c.map(cell => cell?.v);
+
+        const data = table.rows.slice(1).map(row => {
+            const obj = {};
+            row.c.forEach((cell, i) => {
+                obj[attributes[i]] = cell?.v ?? null;
+            });
+            return obj;
+        });
+
+        return data;
+
     } catch (error) {
         console.error(error);
+        return null;
     }
-    return res; 
 }
